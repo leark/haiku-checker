44e41b8590233fb4fc3dd3f0c9645d577e7787ca
Seung Lee <shl7@uw.edu>
2022-06-23 11:58:25 -0700

    Modify initial files for set up
    Co-authored-by: Garrett Hays <garrettlhays@gmail.com>

---
+++ src/haiku.js
@@ -0,0 +1,19 @@
+export default class Haiku {
+  constructor() {}
+}
+
+/*
+
+define what a syllable is
+check if word has correct number of syllables
+
+poem has three lines.
+first line: 5 syllables
+second line: 7 syllables
+third line: 5 syllables
+
+Your logic should verify English syllable rules (and exceptions) one at a time. 
+
+If you successfully complete a Haiku checker, continue to build out your application to randomly generate haikus.
+
+*/
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Haiku Checker',
+      title: 'haiku-checker',
       template: './src/index.html',
       inject: 'body',
     }),
