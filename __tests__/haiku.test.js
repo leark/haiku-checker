bb00604e77c3b1caf84c693f57970130a5421c0f
Seung Lee <shl7@uw.edu>
2022-06-23 13:52:44 -0700

    Red phase of syllable rule 1
    Co-authored-by: Garrett Hays <garrettlhays@gmail.com>

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -1,13 +1,32 @@
-import Haiku from '../src/js/haiku.js';
+import Haiku from '../src/haiku.js';
 
 describe('Haiku', () => {
   let haiku;
+  let line1;
+  let line2;
+  let line3;
 
   beforeEach(() => {
-    haiku = new haiku();
+    line1 = 'My nose is runny';
+    line2 = 'Eyes are puffy, red hue too';
+    line3 = 'Allergy season';
+    haiku = new Haiku(line1, line2, line3);
   });
 
-  test('should correctly create a haiku object', () => {
-    expect(haiku).toEqual(haiku);
+  test('should correctly create a haiku object with given lines', () => {
+    expect(haiku.line1).toEqual(line1);
+    expect(haiku.line2).toEqual(line2);
+    expect(haiku.line3).toEqual(line3);
+  });
+
+  test('should confirm completed haiku has three lines', () => {
+    expect(haiku.lineCount()).toEqual(true);
+    haiku.line3 = '';
+    expect(haiku.lineCount()).toEqual(false);
+  });
+
+  // rule 1
+  test('every syllable has a vowel', () => {
+    expect(haiku.countSyllables('nose')).toEqual(1);
   });
 });
--- src/haiku.js
+++ src/haiku.js
@@ -1,11 +1,49 @@
 export default class Haiku {
-  constructor() {}
+  constructor(line1, line2, line3) {
+    this.line1 = line1;
+    this.line2 = line2;
+    this.line3 = line3;
+  }
+
+  // returns true if all three lines are present
+  lineCount() {
+    if (
+      this.line1.length > 0 &&
+      this.line2.length > 0 &&
+      this.line3.length > 0
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  countSyllables() {
+    // stuff
+  }
 }
 
 /*
 
-define what a syllable is
+constructor
+  option 1
+    line1
+    line2
+    line3
+  
+  option 2
+    words - example "haiku" (2 syllables) - need to pass "haiku" trough a function (to be written) to check against the 7 US syllable rules. Outcome = totalSyllables
+    line - example: line 1: this is a haiku (5 syllables) - need to pass these words through same function as above to identify totalSyllables, then trough another function to confirm line1 requirement of line1Syllables === 5
+
+1. check if poem is 3 lines
+2. check syllable for line 1 and 3 *cause they're same
+3. check syllable for line 2
+
+
 check if word has correct number of syllables
+  method to count syllables in a word
+    countSyllables()
+
 
 poem has three lines.
 first line: 5 syllables
